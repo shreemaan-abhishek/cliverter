d19c48bf9f278bba7961e9bcc2e24a89437f6e51
shreemaan-abhishek <shreemaan.abhishek@gmail.com>
2021-10-03 23:27:14 +0530

    chore: replace printHelpText() with HelpCommand.ts

--- src/cliverter.ts
+++ src/cliverter.ts
@@ -5,27 +5,14 @@ const figlet = require('figlet');
 const args: string[] = process.argv; // Command line arguments
 const inputArgs: string[] = args.slice(2); // Remove first two arguments
 
-const printHelpMessage = () => {
-  const cliverterFiglet = figlet.textSync('Cliverter', {
-    font: 'Standard',
-    horizontalLayout: 'full',
-  });
-  const helpText = `${cliverterFiglet}
-
-    Cliverter is a converter app in a command-line.
-
-    usage:
-      cliverter <command>
-  `
-  console.log(helpText)
-}
+const factoryObject = new CommandFactory();
 
 if(inputArgs.length == 0){
-  printHelpMessage()
+  const commandObject = factoryObject.getCommand("--help");
+  commandObject.run();
 } else if (inputArgs.length > 1) {
   console.log(`Error: Received ${inputArgs.length} arguments but expected 1`)
 } else {
-  const factoryObject = new CommandFactory();
   const commandObject = factoryObject.getCommand(inputArgs[0]);
   commandObject.run();
 }
