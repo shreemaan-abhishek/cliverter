4ac597f9335644938ff1778e07788a949dc2edbd
shreemaan-abhishek <shreemaan.abhishek@gmail.com>
2021-10-05 15:32:19 +0530

    chore: reformat codebase

--- src/cliverter.ts
+++ src/cliverter.ts
@@ -1,19 +1,18 @@
-import { errorMessage, helpMessage } from "./commands/general";
-
-const figlet = require('figlet');
+import { errorMessage, helpMessage } from './commands/general';
 
 const args: string[] = process.argv; // Command line arguments
 const inputArgs: string[] = args.slice(2); // Remove first two arguments
 
-if(inputArgs.length == 0){
-  helpMessage()
+if (inputArgs.length === 0) {
+	helpMessage();
 } else if (inputArgs.length > 1) {
-  console.log(`Error: Received ${inputArgs.length} arguments but expected 1`)
+	console.log(`Error: Received ${inputArgs.length} arguments but expected 1`);
 } else {
-  switch(inputArgs[0]) {
-    case "--help":
-     helpMessage();
-    default:
-     errorMessage();
-   }
+	switch (inputArgs[0]) {
+		case '--help':
+			helpMessage();
+			break;
+		default:
+			errorMessage();
+	}
 }
--- src/commands/general.ts
+++ src/commands/general.ts
@@ -1,14 +1,14 @@
-const figlet = require('figlet')
+import figlet from 'figlet';
 
 export const errorMessage = () => {
-  console.log(`Unrecognized command. See 'cliverter --help'`);
-}
+	console.log(`Unrecognized command. See 'cliverter --help'`);
+};
 
 export const helpMessage = () => {
-  const cliverterFiglet = figlet.textSync('Cliverter', {
-    font: 'Standard',
-    horizontalLayout: 'full',
-  });
-  const helpText = `${cliverterFiglet}\n\nCliverter is a converter app in a command-line.\nusage: cliverter <command>`
-  console.log(helpText)
-}
+	const cliverterFiglet = figlet.textSync('Cliverter', {
+		font: 'Standard',
+		horizontalLayout: 'full',
+	});
+	const helpText = `${cliverterFiglet}\n\nCliverter is a converter app in a command-line.\nusage: cliverter <command>`;
+	console.log(helpText);
+};
