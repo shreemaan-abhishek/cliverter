2cfe13a6156889025060772e14aa4abab028f18b
Mohammed-Saif0 <mohammedsaif9513@gmail.com>
2022-10-01 22:55:36 +0530

    fixed spacing

--- src/cliverter.ts
+++ src/cliverter.ts
@@ -95,6 +95,6 @@ if (inputArgs.length === 0) {
 			supportedCommands[inputArgs[0]].action(inputArgs[1]);
 		}
 	} else {
-		console.log(`${inputArgs[0]} ${(inputArgs[1])?inputArgs[1]:""} is not a recognized command. See 'cliverter --help`);
+		console.log(`${inputArgs[0]} ${(inputArgs[1])?inputArgs[1]+" ":""}is not a recognized command. See 'cliverter --help`);
 	}
 }
